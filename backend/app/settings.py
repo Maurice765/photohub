07e11b44b5ef1fdcbbccc5ff484a1987576ee884
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-06-02 16:36:58 +0200

    configure django to use oracle db

--- backend/app/settings.py
+++ backend/app/settings.py
@@ -82,9 +82,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.oracle",
+        "NAME": "localhost:1521/ORCLPDB1",
+        "USER": "django",
+        "PASSWORD": "admin",
+        "HOST": "",
+        "PORT": "",
     }
 }
 
