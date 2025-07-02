97a6d2fffb534cda5f4242c94088b424d3a76b72
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-03 01:57:13 +0200

    [ADD] test data

---
+++ database/createTestData.sql
@@ -0,0 +1,15 @@
+INSERT INTO "USER" (
+    username,
+    email,
+    password,
+    fname,
+    lname,
+    bio
+) VALUES (
+    'testuser',
+    'test@example.com',
+    'password',
+    'Test',
+    'User',
+    'Dies ist ein Testnutzer für das System.'
+);
