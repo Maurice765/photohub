325c164a0c955b475337224f2a68e49dbfc0416f
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-06 01:57:03 +0200

    chore(angular): regenerate openapi clients

--- frontend/src/app/core/api/model/photoSearchResponse.ts
+++ frontend/src/app/core/api/model/photoSearchResponse.ts
@@ -7,10 +7,10 @@
  * https://openapi-generator.tech
  * Do not edit the class manually.
  */
-import { PhotoSearchResult } from './photoSearchResult';
+import { PhotoSearchResultItem } from './photoSearchResultItem';
 
 
 export interface PhotoSearchResponse { 
-    results: Array<PhotoSearchResult>;
+    results: Array<PhotoSearchResultItem>;
 }
 
