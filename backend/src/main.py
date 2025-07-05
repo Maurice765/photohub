97bff1535de078c1bdb59152007e6ca424db68ea
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-05 19:05:55 +0200

    [ADD] unique ID generation for API routes

--- backend/src/main.py
+++ backend/src/main.py
@@ -1,10 +1,15 @@
 from fastapi import FastAPI
+from fastapi.routing import APIRoute
 from src.photo.router import router as photo_router
+
+def generate_unique_id(route: APIRoute):
+    return route.name
 
 app = FastAPI(
     title="FastAPI Photohub Service",
     description="An API to upload and search photos.",
-    version="1.0.0"
+    version="1.0.0",
+    generate_unique_id_function=generate_unique_id,
 )
 
 app.include_router(photo_router, prefix="/photo", tags=["photo"])
