88b068489b7be92d8317195e3ef1b80bf8f7b378
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-07 00:33:58 +0200

    feat(photoSearch): add photo search routing configuration

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,13 @@
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'photo-search',
+        loadChildren: () =>
+        import('./features/photo-search/photo-search.routes').then(m => m.PHOTO_SEARCH_ROUTES),
+    },
+    {
+        path: '**',
+        redirectTo: 'photo-search',
+    }
+];
---
+++ frontend/src/app/features/photo-search/photo-search.routes.ts
@@ -0,0 +1,10 @@
+import { Routes } from '@angular/router';
+import { PhotoSearchComponent } from './photo-search.component';
+
+
+export const PHOTO_SEARCH_ROUTES: Routes = [
+    {
+        path: '',
+        component: PhotoSearchComponent,
+    },
+];
