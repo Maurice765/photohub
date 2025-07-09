ba78dd01955818100dcae307837aff2c91ffca53
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-09 11:20:15 +0200

    feat(photoSearch): add RGB color picker component

---
+++ frontend/src/app/shared/rgb-color-picker/rgb-color-picker.component.ts
@@ -0,0 +1,77 @@
+import { Component } from "@angular/core";
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR, ReactiveFormsModule } from "@angular/forms";
+import { ColorPickerChangeEvent, ColorPickerModule } from "primeng/colorpicker";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputGroupAddonModule } from "primeng/inputgroupaddon";
+import { InputNumberModule } from "primeng/inputnumber";
+import { RGBColor } from "./models/rgbColor.interface";
+
+@Component({
+    selector: "rgb-color-picker",
+    imports: [
+        FormsModule,
+        ReactiveFormsModule,
+        ColorPickerModule,
+        InputGroupModule,
+        InputGroupAddonModule,
+        InputNumberModule
+    ],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        multi: true,
+        useExisting: RgbColorPickerComponent
+    }],
+    templateUrl: "./rgb-color-picker.component.html",
+    styleUrls: ["./rgb-color-picker.component.css"],
+})
+export class RgbColorPickerComponent implements ControlValueAccessor {
+
+    public rgbColor: RGBColor | null = null;
+    public disabled: boolean = false;
+    public touched: boolean = false;
+
+    public onChange = (rgbColor: RGBColor | null) => { };
+    public onTouched = () => { };
+
+    public onColorPickerChange(event: ColorPickerChangeEvent): void {
+        this.handleColorChange();
+    }
+
+    public onColorInputChange(channel: 'r' | 'g' | 'b', value: number): void {
+        if (!this.rgbColor) {
+            this.rgbColor = { r: 0, g: 0, b: 0 };
+        }
+        this.rgbColor[channel] = value;
+        this.handleColorChange();
+    }
+
+    public writeValue(rgbColor: RGBColor | null): void {
+        this.rgbColor = rgbColor;
+    }
+
+    public registerOnChange(onChange: any): void {
+        this.onChange = onChange;
+    }
+
+    public registerOnTouched(onTouched: any): void {
+        this.onTouched = onTouched;
+    }
+
+    public markAsTouched(): void {
+        if (!this.touched) {
+            this.onTouched();
+            this.touched = true;
+        }
+    }
+
+    public setDisabledState(disabled: boolean): void {
+        this.disabled = disabled;
+    }
+
+    private handleColorChange(): void {
+        this.markAsTouched();
+        if (!this.disabled && this.rgbColor) {
+            this.onChange(this.rgbColor);
+        }
+    }
+}
