de589e1f499a89d1ea138cba7a6a3c3759063558
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-15 00:14:40 +0200

    feat(frontend): add visibility selector component with options and constants

---
+++ frontend/src/app/shared/components/visibility-selector/visibility-selector.component.ts
@@ -0,0 +1,65 @@
+import { Component } from "@angular/core";
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { VISIBILITIES } from "@shared/constants/visibilities.const";
+import { Visibility } from "@shared/enums/visibility.enum";
+import { FloatLabelModule } from "primeng/floatlabel";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputGroupAddonModule } from "primeng/inputgroupaddon";
+import { SelectChangeEvent, SelectModule } from "primeng/select";
+
+@Component({
+    selector: "visibility-selector",
+    imports: [
+        FormsModule,
+        InputGroupModule,
+        InputGroupAddonModule,
+        FloatLabelModule,
+        SelectModule
+    ],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        multi: true,
+        useExisting: VisibilitySelectorComponent
+    }],
+    templateUrl: "./visibility-selector.component.html",
+    styleUrls: ["./visibility-selector.component.css"],
+})
+export class VisibilitySelectorComponent implements ControlValueAccessor {
+    public visibilities = VISIBILITIES;
+    public selectedVisibility: Visibility | null = null;
+    public disabled: boolean = false;
+    public touched: boolean = false;
+
+    public onChange = (visibility: Visibility | null) => { };
+    public onTouched = () => { };
+
+    public onVisibilityChange(event: SelectChangeEvent): void {
+        this.markAsTouched();
+        if (!this.disabled) {
+            this.onChange(this.selectedVisibility);
+        }
+    }
+
+    public writeValue(visibility: Visibility | null): void {
+        this.selectedVisibility = visibility;
+    }
+
+    public registerOnChange(onChange: any): void {
+        this.onChange = onChange;
+    }
+
+    public registerOnTouched(onTouched: any): void {
+        this.onTouched = onTouched;
+    }
+
+    public markAsTouched(): void {
+        if (!this.touched) {
+            this.onTouched();
+            this.touched = true;
+        }
+    }
+
+    public setDisabledState(disabled: boolean): void {
+        this.disabled = disabled;
+    }
+}
