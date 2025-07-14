0d0ed8e26875aa309e108f77e11b6f3c9115976b
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-15 00:13:49 +0200

    feat(frontend): add camera model selector component with options

---
+++ frontend/src/app/shared/components/camera-model-selector/camera-model-selector.component.ts
@@ -0,0 +1,64 @@
+import { Component } from "@angular/core";
+import { ControlValueAccessor, FormsModule, NG_VALUE_ACCESSOR } from "@angular/forms";
+import { FloatLabelModule } from "primeng/floatlabel";
+import { InputGroupModule } from "primeng/inputgroup";
+import { InputGroupAddonModule } from "primeng/inputgroupaddon";
+import { SelectChangeEvent, SelectModule } from "primeng/select";
+import { CAMERA_MODELS } from '@shared/constants/camera-models.const';
+
+@Component({
+    selector: "camera-model-selector",
+    imports: [
+        FormsModule,
+        InputGroupModule,
+        InputGroupAddonModule,
+        FloatLabelModule,
+        SelectModule
+    ],
+    providers: [{
+        provide: NG_VALUE_ACCESSOR,
+        multi: true,
+        useExisting: CameraModelSelectorComponent
+    }],
+    templateUrl: "./camera-model-selector.component.html",
+    styleUrls: ["./camera-model-selector.component.css"],
+})
+export class CameraModelSelectorComponent implements ControlValueAccessor {
+    public cameraModels = CAMERA_MODELS;
+    public selectedCameraModel: string | null = null;
+    public disabled: boolean = false;
+    public touched: boolean = false;
+    
+    public onChange = (cameraModel: string | null) => { };
+    public onTouched = () => { };
+
+    public onCameraModelChange(event: SelectChangeEvent): void {
+        this.markAsTouched();
+        if (!this.disabled) {
+            this.onChange(this.selectedCameraModel);
+        }
+    }
+
+    public writeValue(cameraModel: string | null): void {
+        this.selectedCameraModel = cameraModel;
+    }
+
+    public registerOnChange(onChange: any): void {
+        this.onChange = onChange;
+    }
+
+    public registerOnTouched(onTouched: any): void {
+        this.onTouched = onTouched;
+    }
+
+    public markAsTouched(): void {
+        if (!this.touched) {
+            this.onTouched();
+            this.touched = true;
+        }
+    }
+
+    public setDisabledState(disabled: boolean): void {
+        this.disabled = disabled;
+    }
+}
---
+++ frontend/src/app/shared/constants/camera-models.const.ts
@@ -0,0 +1,6 @@
+export const CAMERA_MODELS = [
+    { name: 'iPhone 11', key: 'iphone11' },
+    { name: 'iPhone 12', key: 'iphone12' },
+    { name: 'Samsung Galaxy S21', key: 'galaxyS21' },
+    { name: 'Google Pixel 6', key: 'pixel6' },
+];
