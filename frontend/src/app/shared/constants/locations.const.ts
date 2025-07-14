d74d12fc85c93245f31059637df4c14411c2b485
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-15 00:14:19 +0200

    feat(frontend): add location selector component with locations constant

---
+++ frontend/src/app/shared/constants/locations.const.ts
@@ -0,0 +1,12 @@
+export const LOCATIONS = [
+    { name: 'Australia', key: 'AU' },
+    { name: 'Brazil', key: 'BR' },
+    { name: 'China', key: 'CN' },
+    { name: 'Egypt', key: 'EG' },
+    { name: 'France', key: 'FR' },
+    { name: 'Germany', key: 'DE' },
+    { name: 'India', key: 'IN' },
+    { name: 'Japan', key: 'JP' },
+    { name: 'Spain', key: 'ES' },
+    { name: 'United States', key: 'US' },
+];
