b069d06c1641118912423d07fc04f48c92d254fe
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-20 15:13:39 +0200

    feat(frontend): add feed feature with routing and component

---
+++ frontend/src/app/features/feed/pages/feed/feed.page.ts
@@ -0,0 +1,10 @@
+import { Component } from "@angular/core";
+
+@Component({
+    selector: 'feed-page',
+    templateUrl: './feed.page.html',
+    styleUrls: ['./feed.page.css'],  
+})
+export class FeedPage {
+
+}
