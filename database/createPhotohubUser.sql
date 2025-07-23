98a48b19e794808e1435c323e06b2ef2e30a13f8
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-23 16:48:29 +0200

    feat(database): add dropTables.sql script to remove existing database objects

--- database/createPhotohubUser.sql
+++ database/createPhotohubUser.sql
@@ -1,23 +1,21 @@
--- Benutzer anlegen
+-- Create a user for the photohub application
 CREATE USER photohub IDENTIFIED BY admin
 DEFAULT TABLESPACE users
 TEMPORARY TABLESPACE temp
 QUOTA UNLIMITED ON users;
 
--- Grundlegende Berechtigungen für photohub
+-- Grant necessary privileges to the photohub user
 GRANT CREATE SESSION TO photohub;
 GRANT CREATE TABLE TO photohub;
 GRANT CREATE SEQUENCE TO photohub;
 GRANT CREATE PROCEDURE TO photohub;
 GRANT CREATE TRIGGER TO photohub;
-
--- Zusätzliche Berechtigungen für Tests
 GRANT CREATE USER TO photohub;
 GRANT ALTER USER TO photohub;
 GRANT DROP USER TO photohub;
 GRANT CREATE TABLESPACE TO photohub;
 GRANT DROP TABLESPACE TO photohub;
-
--- Für Tests mit Views / Materialized Views
 GRANT CREATE VIEW TO photohub;
 GRANT CREATE MATERIALIZED VIEW TO photohub;
+GRANT CREATE INDEX TO photohub;
+GRANT CREATE TYPE TO photohub;
