e8ddff6548864b74401101434b65045cdd6e636d
Maurice Putzger <92910457+Maurice765@users.noreply.github.com>
2025-07-28 14:12:36 +0200

    feat(frontend): add category selector and update photo upload form

--- frontend/src/app/features/photo-upload/components/photo-upload-form/photo-upload-form.component.ts
+++ frontend/src/app/features/photo-upload/components/photo-upload-form/photo-upload-form.component.ts
@@ -1,8 +1,9 @@
 import { ChangeDetectionStrategy, Component, signal } from "@angular/core";
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule, Validators } from "@angular/forms";
-import { VisibilityClientEnum } from "@core/clients/enums/visibility.client-enum";
+import { VisibilityClientEnum } from "@core/clientEnums/visibility.client-enum";
 import { PhotoUploadFormViewModel } from "@features/photo-upload/models/photo-upload-form.view-model";
 import { CameraModelSelectorComponent } from "@shared/components/camera-model-selector/camera-model-selector.component";
+import { CategorySelectorComponent } from "@shared/components/category-selector/category-selector.component";
 import { DatePickerComponent } from "@shared/components/date-picker/date-picker.component";
 import { FileUploadComponent } from "@shared/components/file-upload/file-upload.component";
 import { LocationSelectorComponent } from "@shared/components/location-selector/location-selector.component";
@@ -22,6 +23,7 @@ import { VisibilitySelectorComponent } from "@shared/components/visibility-selec
         LocationSelectorComponent,
         VisibilitySelectorComponent,
         DatePickerComponent,
+        CategorySelectorComponent
     ],
     templateUrl: "./photo-upload-form.component.html",
     styleUrls: ["./photo-upload-form.component.css"],
@@ -34,6 +36,7 @@ export class PhotoUploadFormComponent {
         file: new FormControl<File | null>(null, Validators.required),
         title: new FormControl<string | null>(null, { validators: [Validators.required, Validators.maxLength(255)] }),
         visibility: new FormControl<VisibilityClientEnum | null>(null, Validators.required),
+        categoryId: new FormControl<number | null>(null),
         description: new FormControl<string | null>(null, Validators.maxLength(255)),
         location: new FormControl<string | null>(null, Validators.maxLength(255)),
         cameraModel: new FormControl<string | null>(null, Validators.maxLength(255)),
--- frontend/src/app/features/photo-upload/models/photo-upload-form.view-model.ts
+++ frontend/src/app/features/photo-upload/models/photo-upload-form.view-model.ts
@@ -1,10 +1,11 @@
 import { FormControl } from "@angular/forms";
-import { VisibilityClientEnum } from "@core/clients/enums/visibility.client-enum";
+import { VisibilityClientEnum } from "@core/clientEnums/visibility.client-enum";
 
 export interface PhotoUploadFormViewModel {
     file: FormControl<File | null>;
     title: FormControl<string | null>;
     visibility: FormControl<VisibilityClientEnum | null>;
+    categoryId: FormControl<number | null>;
     description: FormControl<string | null>;
     location: FormControl<string | null>;
     cameraModel: FormControl<string | null>;
